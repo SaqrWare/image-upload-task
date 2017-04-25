c010ce0d569132c04fd06b55dfaee9bf6a326886
Omar Saqr <o.saqr@hotmail.com>
2017-04-25 05:39:18 +0200

    add components & bootstrap sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import  {RouterModule} from '@angular/router'
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ImagesListComponent} from './components/images-list/images-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ImagesListComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot([
+            {
+                path: '',
+                component: ImagesListComponent
+            }
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/image-upload/image-upload.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'images-upload',
+    templateUrl: './images-upload.component.html',
+    styleUrls: ['./images-upload.component.css']
+})
+export class ImagesUploadComponent {
+    title = 'app works!';
+}
